5080f9db670541a0a40ed0eef98c5cd1ab2f4e17
Nader <naderabdalhamed93@gmail.com>
2023-03-13 00:53:34 +0200

    adding smothly navigation between links

--- js/index.js
+++ js/index.js
@@ -15,3 +15,28 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("fixed");
   }
 });
+
+let scrollLinks = document.querySelector(".nav-links");
+scrollLinks.addEventListener("click", (e) => {
+  e.preventDefault();
+  links.classList.remove("show-nav-links");
+  let id = e.target.getAttribute("href").slice(1);
+  let element = document.getElementById(id);
+  let pos;
+  if (navbar.classList.contains("fixed")) {
+    pos = element.offsetTop - 62;
+  } else {
+    pos = element.offsetTop - 124;
+  }
+  if (window.innerWidth < 992) {
+    if (navbar.classList.contains("fixed")) {
+      pos = element.offsetTop - 62;
+    } else {
+      pos = element.offsetTop - 332 - 62;
+    }
+  }
+  window.scrollTo({
+    top: pos,
+    behavior: "smooth",
+  });
+});
